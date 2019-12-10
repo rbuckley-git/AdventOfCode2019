5279af9297780b7ac542eebd2350e36c37cbb1df
Rich Buckley <richb@lt0997.card.co.uk>
2019-12-10 18:52:57 +0000

    Day 10 and the challanges continue. This time through the astorid belt

--- day10.py
+++ day10.py
@@ -2,10 +2,15 @@
 # 10/12/2019
 # Day 10
 #
-# Code refactored for part 2 of the challenge.
+# That was hard. A bit of coding first thing, then at lunch and finished off at the end of the day.
+# Learned about sorting of lists and some set manipulation. 
+# High school trig came back to haunt.
+# Some of the rounding on floats was just to make the debugging easier. There are probably easier ways of doing the trig logic but I brute forced it.
+# Also used all the examples as good test data and made up more of myself.
 #
 
 import unittest
+import math
 
 # "." denotes no astorid. Load file and convert to array of coordinates
 def load_map(filename):
@@ -18,7 +23,6 @@ def load_map(filename):
                 if line[x] != ".":
                     map.append((x,y))
             y+=1
-        #print(map)
         return map
 
 def build_observable(map, location):
@@ -32,25 +36,44 @@ def build_observable(map, location):
         vector = (a[0]-location[0],a[1]-location[1])
         distance = abs(vector[0]) + abs(vector[1])
         b = max(abs(vector[0]),abs(vector[1]))
-        vector = (vector[0]/b,vector[1]/b)  # calculate the normalised vector for this coordinate/location
+        vector = (round(vector[0]/b,4),round(vector[1]/b,4))  # calculate the normalised vector for this coordinate/location
 
         unique_vectors.add(vector)          # for use later, keep a track of unique vectors
         vectors[a] = vector                 # keep a track of what vectors are associated with what node
         distances[a] = distance             # whilst here, calculate and store distance from location against coordinate
     
+    # observables is a set by vector with an array of distances
     observables = {}
     for uv in unique_vectors:
         for a,v in vectors.items():
             if uv == v:
                 if v not in observables:
                     observables[v] = []
-                observables[v].append(distances[a])
+                observables[v].append([a,distances[a]])
 
-    return observables,vectors
+    return observables
+
+def build_vector_and_distance_map(map, location):
+    vectors = {}
+    distances = {}
+    unique_vectors = set()
+    for a in map:
+        if a == location:          # ignore self
+            continue
+
+        vector = (a[0]-location[0],a[1]-location[1])
+        distance = abs(vector[0]) + abs(vector[1])
+        b = max(abs(vector[0]),abs(vector[1]))
+        vector = (round(vector[0]/b,4),round(vector[1]/b,4))  # calculate the normalised vector for this coordinate/location
+
+        unique_vectors.add(vector)          # for use later, keep a track of unique vectors
+        vectors[a] = vector                 # keep a track of what vectors are associated with what node
+        distances[a] = distance             # whilst here, calculate and store distance from location against coordinate
+    
+    return vectors,distances,unique_vectors
 
 def count_observable(map,location):
-    observables,vectors = build_observable(map,location)
-    vectors = vectors   # remove warning
+    observables = build_observable(map,location)
 
     in_line_of_sight = len(observables)
 
@@ -67,8 +90,70 @@ def best_location(map):
 
     return best_location,number_observable
 
-def find_nth_vaporized(map,location):
-    observables,vectors = build_observable(map,location)
+def get_rotation_for_vectors(vectors):
+    rotational_positions = {}
+    for v in vectors:
+        x = v[0]
+        y = v[1]
+        angle = 999
+        if x == 0 and y < 0:
+            angle = 0
+        elif x > 0 and y == 0:
+            angle = 90
+        elif x == 0 and y > 0:
+            angle = 180
+        elif x < 0 and y == 0:
+            angle = 270
+
+        if x != 0 and y != 0:
+            if x > 0:           # Q1,Q2 - right quadrants
+                angle = 90+math.degrees(math.atan(y/x))
+            elif x < 0:         # Q3,Q4 - left half
+                angle = 270+math.degrees(math.atan(y/x))
+
+        rotational_positions[v] = round(angle,4)
+
+    # sort our postions by rotation order so we can just loop round them in order
+    return sorted(rotational_positions.items(), key = lambda kv:(kv[1], kv[0]))
+
+def find_nth_vaporized(map,location,n,debug = False):
+    vectors,distances,unique_vectors = build_vector_and_distance_map(map,location)
+    rotational_positions = get_rotation_for_vectors(unique_vectors)
+
+    if debug:
+        print("rotational ",rotational_positions)
+
+    vaporisation = 0
+    while True:
+        for i in rotational_positions:
+            target_vector = i[0]
+            if debug:
+                print("vectors",vectors)
+                print("key",i,"target vector",target_vector)
+
+            target_positions = []
+            for pos,vector in vectors.items():
+                if vector == target_vector:
+                    target_positions.append((pos,distances[pos]))
+            target_positions = sorted(target_positions, key=lambda kv:kv[1])
+            if debug:
+                print("target_positions",target_positions)
+
+            # do we have any target positions
+            if len(target_positions) > 0:
+                next_target,d = target_positions.pop(0)
+                vectors.pop(next_target)
+                if debug:
+                    print("next target",next_target)
+                vaporisation+=1
+                if vaporisation == n:
+                    return next_target
+
+        # make sure we exit at some point
+        if len(vectors) == 0:
+            break
+
+    return (0,0)
 
 class AdventTestCase(unittest.TestCase):
     def test_count_observable(self):
@@ -104,7 +189,38 @@ class AdventTestCase(unittest.TestCase):
         map = load_map("10.test.5.input.txt")
         self.assertEqual(best_location(map),((11,13),210))
 
+    def test_part2(self):
+        map = [(0,0),(5,-5),(-2,2),(4,4),(5,5),(3,3)]
+        self.assertEqual(find_nth_vaporized(map,(0,0),1),(5,-5))
+        self.assertEqual(find_nth_vaporized(map,(0,0),2),(3,3))
+        self.assertEqual(find_nth_vaporized(map,(0,0),3),(-2,2))
+        self.assertEqual(find_nth_vaporized(map,(0,0),4),(4,4))
+        self.assertEqual(find_nth_vaporized(map,(0,0),5),(5,5))
+
+        map = [(0,0),(5,0)]
+        self.assertEqual(find_nth_vaporized(map,(0,0),1),(5,0))
+
+        map = [(0,0),(0,-5)]
+        self.assertEqual(find_nth_vaporized(map,(0,0),1),(0,-5))
+
+        map = load_map("10.test.5.input.txt")
+        self.assertEqual(find_nth_vaporized(map,(11,13),1),(11,12))
+        self.assertEqual(find_nth_vaporized(map,(11,13),2),(12,1))
+        self.assertEqual(find_nth_vaporized(map,(11,13),3),(12,2))
+        self.assertEqual(find_nth_vaporized(map,(11,13),10),(12,8))
+        self.assertEqual(find_nth_vaporized(map,(11,13),20),(16,0))
+        self.assertEqual(find_nth_vaporized(map,(11,13),50),(16,9))
+        self.assertEqual(find_nth_vaporized(map,(11,13),100),(10,16))
+        self.assertEqual(find_nth_vaporized(map,(11,13),199),(9,6))
+        self.assertEqual(find_nth_vaporized(map,(11,13),200),(8,2))
+        self.assertEqual(find_nth_vaporized(map,(11,13),201),(10,9))
+        self.assertEqual(find_nth_vaporized(map,(11,13),299),(11,1))
+
 if __name__ == '__main__':
     map = load_map("10.input.txt")
     location,best = best_location(map)
     print("part 1 number of observable astorids",best,"from location",location)
+
+    xy = find_nth_vaporized(map,(11,11),200)
+    print("part 2 200th astorid is",xy,"answer",xy[0]*100+xy[1])
+
